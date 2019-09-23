e4a04cf167a1273c7c2c8add2db87b65fdaac82b
Nitin Reddy <reddynitin3@gmail.com>
2019-09-23 10:34:57 +0530

    Changes made the env and Side bar menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import './App.css';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import FileExplorer from './components/FileExplorer';
+import HeaderSliderMenu from './components/HeaderSliderMenu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">Tree View</header>
+    <div className={classes.root}>
+      <HeaderSliderMenu />
       <Container maxWidth="xl">
         <FileExplorer />
       </Container>
